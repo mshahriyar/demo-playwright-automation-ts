907b08a15d0cd652cacfd59165d644c70f7b1594
mshahriyar <shahriyar.abid46@gmail.com>
2026-03-08 23:39:40 +0400

    update report

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 import { config } from './utils/config';
+
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -11,14 +12,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['list'], ['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: config.baseUrl,
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    /* Capture rich artifacts for every run */
+    trace: 'on',
+    video: 'on',
+    screenshot: 'on',
   },
 
   /* Configure projects for major browsers */
