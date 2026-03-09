96cdb487f05f68eec450e2fc5c84a5d67968e0a4
mshahriyar <shahriyar.abid46@gmail.com>
2026-03-09 12:39:47 +0400

    config file update

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: isCI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: isCI ? 1 : 50%,
+  workers: isCI ? 1 : '50%',
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['list'], ['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
